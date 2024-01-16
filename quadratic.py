a04462586c23b16ddb4a05b9ab6aaff29166f9b2
Peter Brown <phbrown@acm.org>
2024-01-16 14:14:55 -0500

    Functions

--- quadratic.py
+++ quadratic.py
@@ -1,20 +1,31 @@
 import math
 
-def main(args: list[str]) -> int:
-    # Characterize the function as a*x**2 + b*x + c = 0
-    # Ask the user for a, b, and c
-    print('Finding roots for a system a*x**2 + b*x + c = 0.')
+def read_quadratic() -> tuple[float, float, float]:
+    """Read the coefficients for a quadratic in the form
+    a*x**2 + b*x + c = 0, and return them as a tuple."""
+    print('Please enter coefficients for a system a*x**2 + b*x + c = 0.')
     a: float = float(input("Please enter a value for a: "))
     b: float = float(input("Please enter a value for b: "))
     c: float = float(input("Please enter a value for c: "))
-    print("System is", a, '*x**2 +', b, '*x +', c, '= 0')
+    return a, b, c
 
-    # find the root(s)
+def find_roots(a: float, b: float, c: float) -> tuple[float, float]:
+    """Find the roots of a quadratic A*x**2 + B*x + C = 0, and return them
+    as a tuple."""
     determinant: float = b**2 - 4*a*c
     #print(determinant) # for testing
     root1: float = (-b + math.sqrt(determinant)) / (2*a)
     root2: float = (-b - math.sqrt(determinant)) / (2*a)
+    return root1, root2
 
+def main(args: list[str]) -> int:
+    # Characterize the function as a*x**2 + b*x + c = 0
+    # Ask the user for a, b, and c
+    a, b, c = read_quadratic()
+    print("System is", a, '*x**2 +', b, '*x +', c, '= 0')
+
+    # find the root(s)
+    root1, root2 = find_roots(a, b, c)    
     # print the root(s)
     print('The roots are', root1, 'and', root2)
 
